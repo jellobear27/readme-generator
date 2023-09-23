3da2f101479f6ec98066a37ba59788ddb793fc25
Janell Smith <119452949+jellobear27@users.noreply.github.com>
2023-09-22 20:16:13 -0400

    git init

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,93 @@
-// TODO: Include packages needed for this application
+// Includes packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Inquirer questions
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your application?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "describe the functionality and motivation of this project.",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "State the languages or technologies used in the project.",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message:
+      "Provide instructions and examples for use. Include screenshots as needed.",
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "select a license applicable to this project.",
+    choices: [
+      "MIT",
+      "APACHIE2.0",
+      "Boost1.0",
+      "GNU AGPLv3",
+      "GNU GPLv3",
+      "The Unlicense",
+      "none",
+    ],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message:
+      "If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so.",
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "Type your full name.",
+  },
+  {
+    type: "input",
+    name: "creator",
+    message: "Write your GitHub username.",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Provide a valid email address",
+  },
+  {
+    type: "input",
+    name: "credits",
+    message:
+      "List your collaborators, if any, with links to their GitHub profiles.",
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// created a function to write README file
 
-// TODO: Create a function to initialize app
-function init() {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully created README.md!");
+    }
+  });
+}
+
+// Created a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const markDownContent = generateMarkdown(answers);
+    writeToFile("README.md", markDownContent);
+  });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,92 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  }
+
+  // Define badge URLs or SVGs for each license
+  const licenseBadges = {
+    MIT: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    "APACHE2.0":
+      "[![License: Apache-2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    // Add badges for other licenses here
+  };
+
+  // Return the badge corresponding to the provided license, or an empty string if not found
+  return licenseBadges[license] || "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return "";
+  }
+  // Define links for each license
+  const licenseLinks = {
+    MIT: "https://opensource.org/licenses/MIT",
+    "APACHE2.0": "https://opensource.org/licenses/Apache-2.0",
+    // Add links for other licenses here
+  };
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  // Return the link corresponding to the provided license, or an empty string if not found
+  return licenseLinks[license] || "";
+}
+
+// Use a template literal to format the license section
+
+function renderLicenceSection() {
+  return `
+    ## License
+    
+    This project is licensed under the [${license} License](${renderLicenseLink(
+    license
+  )}).
+    `;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  by ${data.name}
+  ${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+${renderLicenseLink(data.license)}
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Contact-Me
+* Name - ${data.name}
+* Email - ${data.email}
+* Github - [${data.creator}](https:github.com/${data.creator}/)
+
+## Testing
+\`\`\`
+${data.test}
+
+
+## Credits
+
+${data.credits}
 
 `;
 }
